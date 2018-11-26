9191cdaceca72fcae2c8005552b5347fa28e3fbb
Kevin Akhlaghi <kevin.akhlaghi@gmail.com>
2018-11-26 21:57:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ var renderSearchResult = (result) => {
 var renderSearchResults = (data) => data.items.map( result => renderSearchResult(result))
 
 var searchRepositories = () => {
-  const searchTerms = $('#search').val()
+  const searchTerms = $('#searchTerms').val()
   $.get(`https://api.github.com/search/repositories?q=${searchTerms}`, data => {
       $('#results').html(renderSearchResults(data))
     }).fail(error => {
